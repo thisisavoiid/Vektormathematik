8dde7ad48dd2f42ae7cb0069e113dc843f924ee2
AVØIID <huberjonathan@outlook.de>
2025-11-26 10:45:20 +0100

    some changes and additions.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,13 @@
         /// </param>
         private static void PrintVector(Vector vector)
         {
-            Console.WriteLine($"X: {vector.x}\nY: {vector.y}\nZ: {vector.z}");
+            Console.WriteLine($"X: {vector.X}\nY: {vector.Y}\nZ: {vector.Z}");
         }
 
         static void Main(string[] args)
         {
             Vector v1 = new Vector(x: 5, y: 10, z: 2);
-            Vector v2 = new Vector(x: 2, y: 2, z: 7);
-
+            Vector v2 = new Vector(x: 5, y: 0, z: 2);
 
             Console.WriteLine("Vektorensumme:");
             PrintVector(v1 + v2);
@@ -31,12 +30,20 @@
             Console.WriteLine("Distanz zwischen beiden Vektoren:");
             Console.WriteLine(Vector.DistanceBetween(v1, v2));
 
-            Console.WriteLine("Quadratlänge:");
+            Console.WriteLine("Quadratlänge von Vektor v1:");
             Console.WriteLine(v1.Square());
+
+            Console.WriteLine("Länge von Vektor v1:");
+            Console.WriteLine(v1.Length());
+
+            Console.WriteLine("Einheitsvektor von Vektor v1:");
+            PrintVector(v1.ToUnit());
 
             Console.WriteLine("Skalarprodukt:");
             Console.WriteLine(Vector.ScalarProduct(v1,v2));
 
+            Console.WriteLine("Kreuzprodukt:");
+            PrintVector(Vector.CrossProduct(v1, v2));
         }
     }
 }
--- Vector.cs
+++ Vector.cs
@@ -14,24 +14,24 @@ namespace Vektormathematik
     public struct Vector
     {
         // Coordinate values of the Vector.
-        public float x;
-        public float y;
-        public float z;
+        public float X;
+        public float Y;
+        public float Z;
 
         // Default constructor.
         public Vector()
         {
-            x = 0;
-            y = 0;
-            z = 0;
+            X = 0;
+            Y = 0;
+            Z = 0;
         }
 
         // Overloaded constructor.
         public Vector(float x, float y, float z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
         // Adds two Vector objects by seperately adding every value of the first Vector to those of the second Vector.
@@ -39,9 +39,9 @@ namespace Vektormathematik
             Vector vector_01,
             Vector vector_02
         ) => new Vector(
-            x: vector_01.x + vector_02.x,
-            y: vector_01.y + vector_02.y,
-            z: vector_01.z + vector_02.z
+            x: vector_01.X + vector_02.X,
+            y: vector_01.Y + vector_02.Y,
+            z: vector_01.Z + vector_02.Z
         );
 
         // Substracts two Vector objects by seperately subtracting every value of the first Vector with those of the second Vector.
@@ -49,62 +49,99 @@ namespace Vektormathematik
             Vector vector_01,
             Vector vector_02
         ) => new Vector(
-            x: vector_01.x - vector_02.x,
-            y: vector_01.y + vector_02.y,
-            z: vector_01.z + vector_02.z
+            x: vector_01.X - vector_02.X,
+            y: vector_01.Y + vector_02.Y,
+            z: vector_01.Z + vector_02.Z
         );
 
         // Multiplies the Vector with a scalar by creating a new Vector with the values of the original Vector multiplied with that scalar.
         public static Vector operator *(
             Vector vector,
             float scalar
-        ) => new Vector(x: scalar * vector.x, y: scalar * vector.y, z: scalar * vector.z);
+        ) => new Vector(x: scalar * vector.X, y: scalar * vector.Y, z: scalar * vector.Z);
 
         /// <summary>
-        /// Calculates the distance between two Vector objects.
+        /// Calculates the distance between two <see cref="Vector"/> objects.
         /// </summary>
         /// <param name="vector01">
-        /// First Vector object.
+        /// First <see cref="Vector"/> object.
         /// </param>
         /// <param name="vector02">
-        /// Second Vector object.
+        /// Second <see cref="Vector"/> object.
         /// </param>
         /// <returns>
-        /// The distance between two Vector objects as float.
+        /// The distance between two <see cref="Vector"/> objects as float.
         /// </returns>
         public static float DistanceBetween(Vector vector01, Vector vector02)
         {
             Vector distanceVector = new Vector(
-                x: vector01.x - vector02.x,
-                y: vector01.y - vector02.y,
-                z: vector01.z - vector02.z
+                x: vector01.X - vector02.X,
+                y: vector01.Y - vector02.Y,
+                z: vector01.Z - vector02.Z
             );
 
-            return (float)Math.Sqrt(Math.Sqrt(Math.Pow(distanceVector.x, 2)) + Math.Pow(distanceVector.y, 2) + Math.Pow(distanceVector.z, 2));
+            return (float)Math.Sqrt(Math.Sqrt(Math.Pow(distanceVector.X, 2)) + Math.Pow(distanceVector.Y, 2) + Math.Pow(distanceVector.Z, 2));
         }
 
         /// <summary>
-        /// Calculates the scalar product of two Vector objects.
+        /// Calculates the scalar product of two <see cref="Vector"/> objects.
         /// </summary>
         /// <param name="vector01">
-        /// First Vector object.
+        /// First <see cref="Vector"/> object.
         /// </param>
         /// <param name="vector02">
-        /// Second Vector object.</param>
+        /// Second <see cref="Vector"/> object.</param>
         /// <returns>
-        /// The scalar product of two Vector objects as float.
+        /// The scalar product of two <see cref="Vector"/> objects as float.
         /// </returns>
         public static float ScalarProduct(Vector vector01, Vector vector02)
         {
-            return vector01.x * vector02.x + vector01.y * vector02.y + vector01.z * vector02.z;
+            return vector01.X * vector02.X + vector01.Y * vector02.Y + vector01.Z * vector02.Z;
         }
 
         /// <summary>
-        /// Calculates the square of the Vector object.
+        /// Calculates the square of the <see cref="Vector"/> object.
         /// </summary>
         /// <returns>
-        /// The square of the Vector object as float.
+        /// The square of the <see cref="Vector"/> object as float.
         /// </returns>
-        public float Square() => (float)(x * x + y * y + z * z);
+        public float Square() => (float)(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+
+        /// <summary>
+        /// Calculates the length of the <see cref="Vector"/> object.
+        /// </summary>
+        /// <returns>
+        /// The length of the <see cref="Vector"/> object as float.
+        /// </returns>
+        public float Length() => (float)Math.Sqrt(this.Square());
+
+        /// <summary>
+        /// Calculates the unit <see cref="Vector"/> of the <see cref="Vector"/> object.
+        /// </summary>
+        /// <returns>
+        /// The unit <see cref="Vector"/> of the <see cref="Vector"/> object as float.
+        /// </returns>
+        public Vector ToUnit() => new Vector(X / Length(), Y / Length(), Z / Length());
+
+        /// <summary>
+        /// Calculates the cross product of two <see cref="Vector"/> objects as <see cref="Vector"/>.
+        /// </summary>
+        /// /// <param name="vector01">
+        /// First <see cref="Vector"/> object.
+        /// </param>
+        /// <param name="vector02">
+        /// Second <see cref="Vector"/> object.</param>
+        /// <returns>
+        /// The cross product of two <see cref="Vector"/> objects as <see cref="Vector"/>
+        /// </returns>
+        public static Vector CrossProduct(Vector vector01, Vector vector02)
+        {
+            return new Vector(
+                x: (vector01.Y * vector02.Z - vector01.Z * vector02.Y),
+                y: (vector01.Z * vector02.X - vector01.X * vector02.Z),
+                z: (vector01.X * vector02.Y - vector01.Y * vector02.X)
+            );
+        }
+
     }
 }
